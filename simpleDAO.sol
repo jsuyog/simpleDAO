21e02671b27f8e810e66129b473d6b5bd8454483
suy0g24 <63791642+suy0g24@users.noreply.github.com>
2023-02-17 01:19:59 +0530

    Simple DAO

--- simpleDAO.sol
+++ simpleDAO.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
-contract DAO {
+contract SimpleDAO {
     
     struct Member { // to find if the given address is member and approved by the admin
         bool isMember;
